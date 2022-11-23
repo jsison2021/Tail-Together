31b03ee0be86b6fdc17cf44986186e363755febf
jsison2021 <jsison2021@fau.edu>
2022-11-23 00:36:44 -0500

    Update profile

---
+++ Tail Together/Controllers/Profile/ProfileViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ProfileViewController.swift
+//  Tail Together
+//
+//  Created by Justin on 11/23/22.
+//
+
+import UIKit
+import Parse
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "FirstViewController")
+            
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
